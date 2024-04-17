cd1a9338bb80c9e7d097c1bbac930bbb74c1451b
Satapas T <satapastanachotpaisit@gmail.com>
2024-04-17 14:52:35 +0100

    fix the tailwind configuration

--- kitty_summoner_site/tailwind.config.js
+++ kitty_summoner_site/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx,jsx}",
+
+  '*.{html,js}'], // Like this
+
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
