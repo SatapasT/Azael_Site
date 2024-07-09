b70e47e05346568b60da12ea2be2c8bf474fe12e
Your GitHub SatapasT <satapastanachotpaisit@gmail.com>
2024-07-09 20:34:47 +0100

    added a icon

--- kitty_summoner_site/src/App.tsx
+++ kitty_summoner_site/src/App.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
+import AzaelHeadshot from './assets/images/Azael_Headshot.png';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className={'flex items-start bg-red-950'}>
-      <div className={'flex items-start bg-red-900 h-screen w-20 p-4 border-r-4 border-yellow-600'}>
-        
-    </div>
-      <h1>Counter: {count}</h1>
-      
+      <div className={'flex items-start bg-red-900 h-screen w-28 p-4 border-r-4 border-yellow-600'}>
+        <div className={'flex items-start bg-red-900 border-2 border-slate-600'}>
+          <img src={AzaelHeadshot} alt="Azael Icon Headshot" />
+        </div>     
+      </div>      
     </div>
   );
 }
