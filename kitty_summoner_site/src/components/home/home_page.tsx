319397bb04138d0f85de610d18490496fd30bdeb
Your GitHub SatapasT <satapastanachotpaisit@gmail.com>
2024-07-17 22:30:34 +0100

    fix lint error for home_page.tsx

--- kitty_summoner_site/src/components/home/home_page.tsx
+++ kitty_summoner_site/src/components/home/home_page.tsx
@@ -7,13 +7,15 @@ import telegarmIcon from '../../assets/images/telegarm_icon.png';
 import munchingGrassIcon from '../../assets/images/munching_grass.png';
 
 const HomePage = () => {
+
+const title = " 'Kitty Summoner ' ";
     return (
         <div className='justify marker:items-center w-fit h-auto flex flex-col'>
             <div className='relative top-0 py-2 justify-center flex bg-gradient-to-r items-center from-red-500 via-orange-500 to-yellow-500 bg-clip-text text-8xl font-extrabold text-transparent text-center select-auto animate-glow'>
                 Azael
             </div>
             <div className='relative top-0 justify-center flex bg-gradient-to-r items-center from-red-400 via-orange-400 to-pink-400 bg-clip-text text-1xl font-extrabold text-transparent text-center select-auto'>
-                ' Kitty Summoner  '
+                {title}
             </div>
             <div className='w-full py-10 flex justify-center'>
                 <hr className='w-3/4 border-t-2 border-gradient-to-r from-red-400 via-orange-400 to-pink-400' />
