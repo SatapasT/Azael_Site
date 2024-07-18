49efa7737987b92b13b41f98316941b8d94425a0
Your GitHub SatapasT <satapastanachotpaisit@gmail.com>
2024-07-18 05:50:25 +0100

    worked on the photo page

--- kitty_summoner_site/src/components/photo/photo_page.tsx
+++ kitty_summoner_site/src/components/photo/photo_page.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-
+import { HomePageItem } from "../../constants/home_page_item";
 
 const PhotoPage = () => {
   return (
-    <div className="container md:max bg-red-50 items-start align-top">
-      <img src="https://drive.google.com/thumbnail?id=1Ff4t6qAdTt_zy886hQAggoEybs-T7gyD&sz=w1000" alt=""/>
+    <div className="container w-screen mx-auto px-5 py-2 lg:px-32 lg:pt-24">
+      <div className="columns-1 md:columns-2 lg:columns-3 gap-4">
+        {HomePageItem.map((item) => (
+          <img
+            key={item.photoID}
+            alt="gallery"
+            className="w-full h-auto rounded-lg py-1 px-1"
+            src={`https://drive.google.com/thumbnail?id=${item.photoID}&sz=w1000`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- kitty_summoner_site/src/constants/home_page_item.tsx
+++ kitty_summoner_site/src/constants/home_page_item.tsx
@@ -1,15 +1,247 @@
 export const HomePageItem = [
     {
-        fileName: "newcastle_obtober_1.jpg",
-        title: "Newcastle Meet October 2024",
+    key: 0,
+    photoID: '1af7lOb6-SrWwXKQ1qffEHTQfz-zzkxct'
     },
     {
-        fileName: "newcastle_obtober_2.jpg",
-        title: "Newcastle Meet October 2024",
+    key: 1,
+    photoID: '1-QAIc5cbKk80HRlUAIbSeh7ZeEzHXluN'
     },
     {
-        fileName: "newcastle_obtober_3.jpg",
-        title: "Newcastle Meet October 2024",
-    }
+    key: 2,
+    photoID: '1kIFSnVzVhN8P3K1u-IDWPw-zzWGPQNFg'
+    },
+    {
+    key: 3,
+    photoID: '1Yinj13vkyC6KnTd_6N-jtyo4qc3hZyBD'
+    },
+    {
+    key: 4,
+    photoID: '1k9fttViTaOeBYd7bMiuChbvTRYXZJFFK'
+    },
+    {
+    key: 5,
+    photoID: '1vvmRYkSn-P7tq22JC-Ym2uw3CTXaiLoE'
+    },
+    {
+    key: 6,
+    photoID: '1CpDSQLBiobZ1hCLQ6a409nrwCa5d9LRT'
+    },
+    {
+    key: 7,
+    photoID: '1WlYoFCpQPsFlKBMsrlmR3n_tGWy1lwyI'
+    },
+    {
+    key: 8,
+    photoID: '1vaT5A-ElY7GMkOkl1Ze2PxQH1RZ0O1LN'
+    },
+    {
+    key: 9,
+    photoID: '1IxLcBTOjVP5uKSzHJKqM28L7BsBEbN1Z'
+    },
+    {
+    key: 10,
+    photoID: '1yGC5by9KvHFpoN5bZ28ARxHhbVuLksn6'
+    },
+    {
+    key: 11,
+    photoID: '1wraMP0GmHKDawXj0BuY97Wigx_IMD0e0'
+    },
+    {
+    key: 12,
+    photoID: '14HMLWAYSuWvv0zqpxzKQu3ThgoH42Zas'
+    },
+    {
+    key: 13,
+    photoID: '1VcKkahwB4B0APiW1LStVBTaRC9fDx1U-'
+    },
+    {
+    key: 14,
+    photoID: '1M1oT5jHFI713zgdkSNJdNYp6M791PgKD'
+    },
+    {
+    key: 15,
+    photoID: '1BVX32l2749Xb6KIJkvZoLrVJh3Ej1BYp'
+    },
+    {
+    key: 16,
+    photoID: '1nHx4HZt3vuyUvMaZF6G74JKgafggk4m4'
+    },
+    {
+    key: 17,
+    photoID: '10uv60UndUOiLbM-3INtPkcsz4OKeX7_X'
+    },
+    {
+    key: 18,
+    photoID: '1Y8qZBkFwBd4iu0gc2NNFBsAmSt3icrzU'
+    },
+    {
+    key: 19,
+    photoID: '1dIT_rArTr2pKw3hOP8vpkzlCzyNSUfnQ'
+    },
+    {
+    key: 20,
+    photoID: '1ZUu4dA-OWqTKQ9oIrEbPuA2_Nmsi8B-Q'
+    },
+    {
+    key: 21,
+    photoID: '1spa3MB19dp7GuRWgNOagc7A4_fknW_U4'
+    },
+    {
+    key: 22,
+    photoID: '1qEBKS2gQwVOz-kNFrgYolZVu5otyvJqJ'
+    },
+    {
+    key: 23,
+    photoID: '1GKxSG_2bxWPvGr2BxV3Z1ruI5uPaWbH9'
+    },
+    {
+    key: 24,
+    photoID: '1gR3_wvJbTR_RuN_ji-boH8t60A5QiGh9'
+    },
+    {
+    key: 25,
+    photoID: '1JYLeyBWgbhaVHyUZveq4vwVklJI23V30'
+    },
+    {
+    key: 26,
+    photoID: '1BRvsOKnt_0vfOhrr_p-UL2P4hrTwKnmp'
+    },
+    {
+    key: 27,
+    photoID: '1T5cQ4BY_1B5eAm4ktNB6bkBNjemuMmag'
+    },
+    {
+    key: 28,
+    photoID: '1hJUturfqc2YYabpHXNaURoH96RQoTQqo'
+    },
+    {
+    key: 29,
+    photoID: '1pMHqRAszHLzMn0JegAoiT7FAQ_Mk0_-o'
+    },
+    {
+    key: 30,
+    photoID: '1fe6HUaCHnnw5SeX-ETV4feqiDwWMHIxg'
+    },
+    {
+    key: 31,
+    photoID: '1G91XRY0oCc2FIIIIrimp5S2AORGjfz8K'
+    },
+    {
+    key: 32,
+    photoID: '1yWSk4FPdUt1Dsk9vmFT3bMXAjKrq-g-w'
+    },
+    {
+    key: 33,
+    photoID: '1XHa6HJb8FAVIaQKuYNEgB6cQR8HgU1PW'
+    },
+    {
+    key: 34,
+    photoID: '1MwZidTBYr52zOiUtMce70jlLGqbGEjRT'
+    },
+    {
+    key: 35,
+    photoID: '1YqwMmdn9R_JcICqp5HTC44lcirQJPPrg'
+    },
+    {
+    key: 36,
+    photoID: '1yzs6mXCt1Xuu5XzOs8hxYTJtaQn_eV1Y'
+    },
+    {
+    key: 37,
+    photoID: '1ZA0WijBdquw3Wg8sHsYt1-5xB2QwD-ha'
+    },
+    {
+    key: 38,
+    photoID: '1dWFtqJwDacgBZ2xoVdLDvZI2kxDFmyhT'
+    },
+    {
+    key: 39,
+    photoID: '1lL1veusX6MoH59TsTLLpP4-jmO9WPm2D'
+    },
+    {
+    key: 40,
+    photoID: '1EklqO9M-Wmn4Sk-xD2ujkE_sGGqgG-kX'
+    },
+    {
+    key: 41,
+    photoID: '1-38CBBZ1chAYMu7wqeyXERN3Tgd4Jz0E'
+    },
+    {
+    key: 42,
+    photoID: '13ZxozcnYV3c2C4V_H_E_u9Ii0lrboPWj'
+    },
+    {
+    key: 43,
+    photoID: '17dvxUFQjbGHz3GQ2qICfiX_SPs6bGyng'
+    },
+    {
+    key: 44,
+    photoID: '192s3lH8cO9SBrwTPxeSNEPB7MxI_-bO0'
+    },
+    {
+    key: 45,
+    photoID: '1BLoQ_FFbbzYpO39iElVn0PpJkm3SYkAa'
+    },
+    {
+    key: 46,
+    photoID: '1DBO-P9Zx9h8Z0nXFUSX1f4ND4Hss8eOm'
+    },
+    {
+    key: 47,
+    photoID: '1EXnRBmfKkkzshl-puK2e5c5-UYdUWXn8'
+    },
+    {
+    key: 48,
+    photoID: '1KWi9yjvUSfNDcTtG5Zr0DErNQNblq3DQ'
+    },
+    {
+    key: 49,
+    photoID: '1REGx6vV1eFx4pA73btgrRclKZs8RHZUh'
+    },
+    {
+    key: 50,
+    photoID: '1UouqDtm61El37VGnMYrOtJK-UVTYDq4a'
+    },
+    {
+    key: 51,
+    photoID: '1UqMSpufD0_Ul5zBRPldnolMwpk4NLXlQ'
+    },
+    {
+    key: 52,
+    photoID: '1UzgJh-FmUmSN5XZzkO2HQD4mA8_ThZfy'
+    },
+    {
+    key: 53,
+    photoID: '1_PFfJrQvyoEvmEh9Dr5uYxhoQrgjZU_y'
+    },
+    {
+    key: 54,
+    photoID: '1hzRKWwOn09dpHhTennwHL8SH_51htKwd'
+    },
+    {
+    key: 55,
+    photoID: '1im51e2oPrIaWBegLBKIQuBGLsfqymjfH'
+    },
+    {
+    key: 56,
+    photoID: '1jKOnkAEgYfD9w9Uyzm3DwALGxF194lxj'
+    },
+    {
+    key: 57,
+    photoID: '1oawZeBeecMN-yIlkY0gSqM48flA_CBrH'
+    },
+    {
+    key: 58,
+    photoID: '1onOegnrOLuk4S-iCLPKP5VArykFlOohB'
+    },
+    {
+    key: 59,
+    photoID: '1opxq1stCtCOZmf8MWVuGJxUbDCF3UP2V'
+    },
+    {
+    key: 60,
+    photoID: '1uPgf5YcEzsjJl4eIr-Pb4rfmJKVI6VMM'
+    },
     ];
     
