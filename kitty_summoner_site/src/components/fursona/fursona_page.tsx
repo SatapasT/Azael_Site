37cbfbb973880f5f79244a0de23aae5a78d412f0
Your GitHub SatapasT <satapastanachotpaisit@gmail.com>
2024-07-19 01:08:59 +0100

    improve robustness of the site

--- kitty_summoner_site/src/components/fursona/fursona_page.tsx
+++ kitty_summoner_site/src/components/fursona/fursona_page.tsx
@@ -56,61 +56,62 @@ const FursonaPage = () => {
   }
 
   return (
-    <div className='flex flex-col md:flex-row items-center justify-center w-full h-full'>
-      <div className='flex flex-col items-center flex-1'>
-        <div className='py-2 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 bg-clip-text text-8xl font-extrabold text-transparent text-center select-auto animate-glow'>
-          Azael
-        </div>
-        
-        <div className='py-2 bg-gradient-to-r from-yellow-400 via-orange-400 to-red-400 bg-clip-text text-2xl font-extrabold text-transparent text-center'>
-          The Demon Goat
-        </div>
-        <div className='flex flex-row items-center justify-center w-full h-full'>
-          <img
-            src={Sticker1}
-            title='Sip Tea'
-            className='w-1/6 h-auto'
-          />
-          <img
-            src={Sticker2}
-            title='Cry'
-            className='w-1/6 h-auto'
-          />
-          <img
-            src={Sticker3}
-            title='Munching Grass'
-            className='w-1/6 h-auto'
-          />
-          <img
-            src={Sticker4}
-            title='Sweating'
-            className='w-1/6 h-auto'
-          />
-          <img
-            src={Sticker5}
-            title='Thumb Up'
-            className='w-1/6 h-auto'
-          />
-        </div>
-        <a href="https://t.me/addstickers/Azael_Goat" target="_blank" rel="noopener noreferrer">
-          <div className="py-10 flex justify-center font-extrabold">
-            <button type="button" className="flex items-center text-white bg-gradient-to-br from-red-500 to-orange-500 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-              <img src={munchingGrassIcon} alt="Munching Grass Sticker" className="w-6 h-6 mr-2" />
-              <span>Get Azael Telegram Sticker!</span>
-            </button>
+    <div className='flex items-center justify-center w-full h-fill'>
+      <div className='flex flex-col md:flex-row items-center justify-center w-full h-full py-12'>
+        <div className='flex flex-col items-center flex-1'>
+          <div className='py-2 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 bg-clip-text text-8xl font-extrabold text-transparent text-center select-auto animate-glow'>
+            Azael
           </div>
-        </a>
-      </div>
-      <div className='flex flex-col items-center justify-center w-full h-full relative flex-1'>
-        <a href={RefSheet} download="ReferenceSheet.png" className='text-white'>
-          <img
-            src={RefSheet}
-            title='Reference Sheet'
-            className='w-fill h-auto hover:scale-110 transition-transform duration-300'
-          />
-        </a>
-        <div className='text-white text-center mt-2'>
-          Click to download!
+          <div className='py-2 bg-gradient-to-r from-yellow-400 via-orange-400 to-red-400 bg-clip-text text-2xl font-extrabold text-transparent text-center'>
+            The Demon Goat
+          </div>
+          <div className='flex flex-row items-center justify-center w-full'>
+            <img
+              src={Sticker1}
+              title='Sip Tea'
+              className='w-1/6 h-auto'
+            />
+            <img
+              src={Sticker2}
+              title='Cry'
+              className='w-1/6 h-auto'
+            />
+            <img
+              src={Sticker3}
+              title='Munching Grass'
+              className='w-1/6 h-auto'
+            />
+            <img
+              src={Sticker4}
+              title='Sweating'
+              className='w-1/6 h-auto'
+            />
+            <img
+              src={Sticker5}
+              title='Thumb Up'
+              className='w-1/6 h-auto'
+            />
+          </div>
+          <a href="https://t.me/addstickers/Azael_Goat" target="_blank" rel="noopener noreferrer">
+            <div className="py-10 flex justify-center font-extrabold">
+              <button type="button" className="flex items-center text-white bg-gradient-to-br from-red-500 to-orange-500 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                <img src={munchingGrassIcon} alt="Munching Grass Sticker" className="w-6 h-6 mr-2" />
+                <span>Get Azael Telegram Sticker!</span>
+              </button>
+            </div>
+          </a>
+        </div>
+        <div className='flex flex-col items-center justify-center w-full relative flex-1'>
+          <a href={RefSheet} download="ReferenceSheet.png" className='text-white'>
+            <img
+              src={RefSheet}
+              title='Reference Sheet'
+              className='w-fill h-auto hover:scale-110 transition-transform duration-300'
+            />
+          </a>
+          <div className='text-white text-center mt-2'>
+            Click to download!
+          </div>
         </div>
       </div>
     </div>
--- kitty_summoner_site/src/components/home/home_page.tsx
+++ kitty_summoner_site/src/components/home/home_page.tsx
@@ -5,7 +5,7 @@ import tiktokIcon from '../../assets/images/tiktok_icon.png';
 import instagarmIcon from '../../assets/images/instagram_icon.png';
 import telegarmIcon from '../../assets/images/telegarm_icon.png';
 import munchingGrassIcon from '../../assets/images/munching_grass.png';
-import AzaelHeadshotImage from '../../assets/images/azael_headshot.png'
+import AzaelHeadshotImage from '../../assets/images/azael_headshot.png';
 
 const LoadingSpinner = () => (
     <div className="flex items-center justify-center w-full h-full">
@@ -57,38 +57,39 @@ const HomePage: React.FC = () => {
     }
 
     return (
-        
-        <div className='flex flex-col items-center justify-center w-full h-full'>
-            <img
-                        src={AzaelHeadshotImage}
-                        alt='Azael Icon'
-                        className='p-2 box-border w-1/5 h-auto'
-                    />
-            <div className='py-2 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 bg-clip-text text-8xl font-extrabold text-transparent text-center select-auto animate-glow'>
-                Azael
-            </div>
-            <div className='bg-gradient-to-r from-red-400 via-orange-400 to-pink-400 bg-clip-text text-1xl font-extrabold text-transparent text-center select-auto'>
-                {title}
-            </div>
-            <div className='w-1/2 py-10 flex justify-center'>
-                <hr className='w-3/4 border-t-2 border-gradient-to-r from-red-400 via-orange-400 to-pink-400' />
-            </div>
-            <div className='flex flex-row justify-center items-center space-x-4'>
-                <a href="https://x.com/Azael_Goat" target="_blank" rel="noopener noreferrer">
-                    <img src={xIcon} alt="X Social Media" className="enlarge uniform-size" />
-                </a>
-                <a href="https://www.tiktok.com/@azael_goat" target="_blank" rel="noopener noreferrer">
-                    <img src={tiktokIcon} alt="TikTok Social Media" className="enlarge uniform-size" />
-                </a>
-                <a href="https://www.instagram.com/azael_goat/" target="_blank" rel="noopener noreferrer">
-                    <img src={instagarmIcon} alt="TikTok Social Media" className="enlarge uniform-size" />
-                </a>
-                <a href="https://t.me/KittySummoner" target="_blank" rel="noopener noreferrer">
-                    <img src={telegarmIcon} alt="TikTok Social Media" className="enlarge uniform-size" />
-                </a>
-            </div>
-            <div className='text-red-600 font-extrabold py-5'>
-                SOME PAGE IS STILL UNDER WORK, MANY OF THE STUFF NOT ADDED YET
+        <div className='flex items-center justify-center w-full h-fill'>
+            <div className='flex flex-col items-center justify-center w-full h-full py-12'>
+                <img
+                    src={AzaelHeadshotImage}
+                    alt='Azael Icon'
+                    className='p-2 box-border w-1/5 h-auto'
+                />
+                <div className='py-2 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500 bg-clip-text text-8xl font-extrabold text-transparent text-center select-auto animate-glow'>
+                    Azael
+                </div>
+                <div className='bg-gradient-to-r from-red-400 via-orange-400 to-pink-400 bg-clip-text text-1xl font-extrabold text-transparent text-center select-auto'>
+                    {title}
+                </div>
+                <div className='w-1/2 py-10 flex justify-center'>
+                    <hr className='w-3/4 border-t-2 border-gradient-to-r from-red-400 via-orange-400 to-pink-400' />
+                </div>
+                <div className='flex flex-row justify-center items-center space-x-4'>
+                    <a href="https://x.com/Azael_Goat" target="_blank" rel="noopener noreferrer">
+                        <img src={xIcon} alt="X Social Media" className="enlarge uniform-size" />
+                    </a>
+                    <a href="https://www.tiktok.com/@azael_goat" target="_blank" rel="noopener noreferrer">
+                        <img src={tiktokIcon} alt="TikTok Social Media" className="enlarge uniform-size" />
+                    </a>
+                    <a href="https://www.instagram.com/azael_goat/" target="_blank" rel="noopener noreferrer">
+                        <img src={instagarmIcon} alt="Instagram Social Media" className="enlarge uniform-size" />
+                    </a>
+                    <a href="https://t.me/KittySummoner" target="_blank" rel="noopener noreferrer">
+                        <img src={telegarmIcon} alt="Telegram Social Media" className="enlarge uniform-size" />
+                    </a>
+                </div>
+                <div className='text-red-600 font-extrabold py-5'>
+                    SOME PAGE IS STILL UNDER WORK, MANY OF THE STUFF NOT ADDED YET
+                </div>
             </div>
         </div>
     );
