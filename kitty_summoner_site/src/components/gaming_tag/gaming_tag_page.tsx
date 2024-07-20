de5e5cc22511c6d36cfc5b6bd73fbeb1d8939ffc
Your GitHub SatapasT <satapastanachotpaisit@gmail.com>
2024-07-20 23:22:41 +0100

    imporve the profermoance of the site

--- kitty_summoner_site/src/components/gaming_tag/gaming_tag_page.tsx
+++ kitty_summoner_site/src/components/gaming_tag/gaming_tag_page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LeagueIcon from '../../assets/images/league_icon.webp';
 import FinalFantasyIcon from '../../assets/images/final_fantasy_xiv_icon.webp';
 import SteamIcon from '../../assets/images/steam_icon.webp';
@@ -9,7 +9,56 @@ import FortniteIcon from '../../assets/images/fortnite_icon.webp';
 import SmashUltimateIcon from '../../assets/images/smash_ultimate_icon.webp';
 import VRChatIcon from '../../assets/images/VRChat_Icon.webp';
 
+const LoadingSpinner = () => (
+  <div className="flex items-center justify-center w-full h-screen">
+    <svg
+      role="status"
+      className="w-16 h-16 text-gray-200 animate-spin fill-blue-600"
+      viewBox="0 0 100 101"
+      fill="none"
+    >
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="currentColor"
+      />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentFill"
+      />
+    </svg>
+  </div>
+);
+
 const GamingTagPage:React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const imagePromises = [
+      LeagueIcon,
+      FinalFantasyIcon,
+      SteamIcon,
+      ValorantIcon,
+      ClashOfClanIcon,
+      OverwatchIcon,
+      FortniteIcon,
+      SmashUltimateIcon,
+      VRChatIcon,
+    ].map(src => {
+      return new Promise(resolve => {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve;
+        img.onerror = resolve;
+      });
+    });
+
+    Promise.all(imagePromises).then(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <div className="flex flex-col items-center justify-start w-full min-h-screen pb-10">
       <div className="py-2 text-white bg-clip-text text-8xl font-extrabold text-transparent text-center mt-4">
@@ -21,7 +70,7 @@ const GamingTagPage:React.FC = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 p-4 h-1/2 py-8">
         <div className="h-1/5 md:ml-4">
           <a href="https://www.op.gg/summoners/euw/Kitty%20Summoner-Meow" target="_blank" rel="noopener noreferrer">
-            <img loading='lazy' src={LeagueIcon} alt="League of Legends" className="w-1/2 h-auto mx-auto" />
+            <img src={LeagueIcon} alt="League of Legends" className="w-1/2 h-auto mx-auto" />
           </a>
           <div className="text-white text-center text-2xl">
             League of Legends
@@ -32,7 +81,7 @@ const GamingTagPage:React.FC = () => {
         </div>
         <div className="h-1/5 md:ml-4">
           <a href="https://eu.finalfantasyxiv.com/lodestone/character/36651684/" target="_blank" rel="noopener noreferrer">
-            <img loading='lazy' src={FinalFantasyIcon} alt="Final Fantasy XIV" className="w-1/2 h-auto mx-auto" />
+            <img src={FinalFantasyIcon} alt="Final Fantasy XIV" className="w-1/2 h-auto mx-auto" />
           </a>
           <div className="text-white text-center text-2xl">
             Final Fantasy XIV
@@ -48,7 +97,7 @@ const GamingTagPage:React.FC = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 p-4 h-1/2 py-8">
         <div className="h-1/5 md:ml-4 ">
           <a href="https://vrchat.com/home/user/usr_87bd4764-441e-40d7-ae6c-c82b91165f43" target="_blank" rel="noopener noreferrer">
-            <img loading='lazy' src={VRChatIcon} alt="VRChat" className="w-1/2 h-auto mx-auto" />
+            <img src={VRChatIcon} alt="VRChat" className="w-1/2 h-auto mx-auto" />
           </a>
           <div className="text-white text-center text-2xl ">
             VRChat
@@ -59,7 +108,7 @@ const GamingTagPage:React.FC = () => {
         </div>
         <div className="h-1/5 md:ml-4 ">
           <a href="https://steamcommunity.com/id/KittySummoner/" target="_blank" rel="noopener noreferrer">
-            <img loading='lazy' src={SteamIcon} alt="Steam" className="w-1/2 h-auto mx-auto" />
+            <img src={SteamIcon} alt="Steam" className="w-1/2 h-auto mx-auto" />
           </a>
           <div className="text-white text-center text-2xl ">
             Steam
@@ -71,7 +120,7 @@ const GamingTagPage:React.FC = () => {
 
         <div className="h-1/5 md:ml-4 ">
           <a href="https://tracker.gg/valorant/profile/riot/Kitty%20Summoner%23Sugar/overview" target="_blank" rel="noopener noreferrer">
-            <img loading='lazy' src={ValorantIcon} alt="Valorant" className="w-1/2 h-auto mx-auto" />
+            <img src={ValorantIcon} alt="Valorant" className="w-1/2 h-auto mx-auto" />
           </a>
           <div className="text-white text-center text-2xl ">
             Valorant
@@ -83,7 +132,7 @@ const GamingTagPage:React.FC = () => {
 
         <div className="h-1/5 md:ml-4 ">
           <a href="https://www.clashofstats.com/players/gentlemen-gnar-9LPPQJCG/army#tabs" target="_blank" rel="noopener noreferrer">
-            <img loading='lazy' src={ClashOfClanIcon} alt="Clash of Clans" className="w-1/2 h-auto mx-auto" />
+            <img src={ClashOfClanIcon} alt="Clash of Clans" className="w-1/2 h-auto mx-auto" />
           </a>
           <div className="text-white text-center text-2xl ">
             Clash of Clans
@@ -95,7 +144,7 @@ const GamingTagPage:React.FC = () => {
 
         <div className="h-1/5 md:ml-4 ">
           <a>
-            <img loading='lazy' src={OverwatchIcon} alt="Overwatch" className="w-1/2 h-auto mx-auto" />
+            <img src={OverwatchIcon} alt="Overwatch" className="w-1/2 h-auto mx-auto" />
           </a>
           <div className="text-white text-center text-2xl ">
             Overwatch
@@ -107,7 +156,7 @@ const GamingTagPage:React.FC = () => {
 
         <div className="h-1/5 md:ml-4 ">
           <a href="https://fortnitetracker.com/profile/all/Kitty%20Summoner" target="_blank" rel="noopener noreferrer">
-            <img loading='lazy' src={FortniteIcon} alt="Fortnite" className="w-1/2 h-auto mx-auto" />
+            <img src={FortniteIcon} alt="Fortnite" className="w-1/2 h-auto mx-auto" />
           </a>
           <div className="text-white text-center text-2xl ">
             Fortnite
@@ -119,7 +168,7 @@ const GamingTagPage:React.FC = () => {
 
         <div className="h-1/5 md:ml-4 ">
           <a href="https://www.pgstats.com/ultimate/player/Kitty%20Summoner?id=S3824793" target="_blank" rel="noopener noreferrer">
-            <img loading='lazy' src={SmashUltimateIcon} alt="Smash Ultimate" className="w-1/2 h-auto mx-auto" />
+            <img src={SmashUltimateIcon} alt="Smash Ultimate" className="w-1/2 h-auto mx-auto" />
           </a>
           <div className="text-white text-center text-2xl ">
             Smash Ultimate
--- kitty_summoner_site/src/constants/art_page_item.tsx
+++ kitty_summoner_site/src/constants/art_page_item.tsx
@@ -1,80 +1,107 @@
+import AzaelHeadshot from "../assets/arts_images/azael_headshot.png";
+import AzaelReferenceSheets from "../assets/arts_images/azael_reference_sheets.png";
+import BiPrideIcon from "../assets/arts_images/bi_pride_icon.png";
+import CultOfTheGoat from "../assets/arts_images/cult_of_the_goat.png";
+import DemonSlayerCosplay from "../assets/arts_images/demon_slayer_cosplay.png";
+import GateToHell from "../assets/arts_images/gate_to_hell.jpg";
+import GoatSticker from "../assets/arts_images/goat_sticker.png";
+import GoofyIcon from "../assets/arts_images/goofy_icon.png";
+import HoodIcon from "../assets/arts_images/hood_icon.png";
+import Maid from "../assets/arts_images/maid.png";
+import SillyGoat from "../assets/arts_images/silly_goat.jpeg";
+import SparkleIcon from "../assets/arts_images/sparkle_icon.jpg";
+import TooCoolForSchool from "../assets/arts_images/too_cool_for_school.png";
+
 export const ArtPageItem = [
     {
         key: 0,
         photoID: '12NoWGuc1_9Crp3wph4CeI6H3ae7DOs1X',
         artist: 'X | Twitter @NiixARTs',
         link: 'https://x.com/NiixARTs',
+        imageName: GateToHell,
     },
     {
         key: 1,
         photoID: '13jmyyJadqU5DNIN_EANOB7nSJtw0hdF_',
         artist: 'X | Twitter @HirscheSly',
         link: 'https://x.com/HirscheSly',
+        imageName: SparkleIcon,
     },
     {
         key: 2,
         photoID: '14_I2bj_uCGpDsnwqsUM8RJuNXQY4DYYl',
         artist: 'X | Twitter @Pentaagonkaa',
         link: 'https://x.com/Pentaagonkaa',
+        imageName: Maid,
     },
     {
         key: 3,
         photoID: '1B7vJ2o_GpIT0kcqYEyPgbLi728-3pA_2',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
+        imageName: HoodIcon,
     },
     {
         key: 4,
         photoID: '1QeQWTTj2GM6G7bsCG-H-KpKnU-RFXNhI',
         artist: 'X | Twitter @Beelzeryon',
         link: 'https://x.com/Beelzeryon',
+        imageName: DemonSlayerCosplay,
     },
     {
         key: 5,
         photoID: '1UrAufq6bNPLSEQ6GTo1bLXKA3KOC00Qr',
         artist: 'X | Twitter @NiixARTs',
         link: 'https://x.com/NiixARTs',
+        imageName: TooCoolForSchool,
     },
     {
         key: 6,
         photoID: '1EvL7mUMtT07RCe9hv9_MaHseNW9Hcuat',
         artist: 'X | Twitter @foxynalie',
         link: 'https://x.com/foxynalie',
+        imageName: CultOfTheGoat,
     },
     {
         key: 7,
         photoID: '1GqBbR0lr79sT0eJICiZCmjKAHwkfqw_g',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
+        imageName: GoatSticker,
     },
     {
         key: 8,
         photoID: '1Ipi_fjyaX2ma827XKpwCGWUga3_jz2l9',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
+        imageName: AzaelReferenceSheets,
     },
     {
         key: 9,
         photoID: '1JIGxYjARSef5Mor86WCjuM3XGthI2ngi',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
+        imageName: GoofyIcon,
     },
     {
         key: 10,
         photoID: '1Pk8jWzMrqCGNdUA3Bo7onXUE_lIHXNCX',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
+        imageName: BiPrideIcon,
     },
     {
         key: 11,
         photoID: '1M42J2sPGUMLbX1OST5Q62VmMd3z5pKbN',
         artist: 'X | Twitter @CrowParadeArt',
         link: 'https://x.com/CrowParadeArt',
+        imageName: SillyGoat,
     },
     {
         key: 12,
         photoID: '1_yHyU7WNKH2KD36yOi0JbLVRkrv9mv4t',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
+        imageName: AzaelHeadshot,
     },
 ];
