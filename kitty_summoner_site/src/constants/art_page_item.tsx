72d8e3ce522863f0a8cfc10da2aafccae2b3eded
Your GitHub SatapasT <satapastanachotpaisit@gmail.com>
2024-10-22 21:06:21 +0100

    improve formatting and added a image

--- kitty_summoner_site/src/constants/art_page_item.tsx
+++ kitty_summoner_site/src/constants/art_page_item.tsx
@@ -11,6 +11,7 @@ import Maid from "../assets/arts_images/maid.webp";
 import SillyGoat from "../assets/arts_images/silly_goat.webp";
 import SparkleIcon from "../assets/arts_images/sparkle_icon.webp";
 import TooCoolForSchool from "../assets/arts_images/too_cool_for_school.webp";
+import RoomateWithPad from "../assets/arts_images/roomate_with_pad.webp";
 
 import AzaelHeadshotHD from "../assets/arts_images/azael_headshot_hd.png";
 import AzaelReferenceSheetsHD from "../assets/arts_images/azael_reference_sheets_hd.png";
@@ -25,107 +26,89 @@ import MaidHD from "../assets/arts_images/maid_hd.png";
 import SillyGoatHD from "../assets/arts_images/silly_goat_hd.jpeg";
 import SparkleIconHD from "../assets/arts_images/sparkle_icon_hd.jpg";
 import TooCoolForSchoolHD from "../assets/arts_images/too_cool_for_school_hd.png";
+import RoomateWithPadHD from "../assets/arts_images/roomate_with_pad_hd.png";
+
 
 export const ArtPageItem = [
     {
-        key: 0,
-        photoID: '12NoWGuc1_9Crp3wph4CeI6H3ae7DOs1X',
+        artist: 'X | Twitter @LoveIock_',
+        link: 'https://x.com/LoveIock_',
+        imageName: RoomateWithPad,
+        imageHD: RoomateWithPadHD,
+    },
+    {
         artist: 'X | Twitter @NiixARTs',
         link: 'https://x.com/NiixARTs',
         imageName: GateToHell,
         imageHD: GateToHellHD,
     },
     {
-        key: 1,
-        photoID: '13jmyyJadqU5DNIN_EANOB7nSJtw0hdF_',
         artist: 'X | Twitter @HirscheSly',
         link: 'https://x.com/HirscheSly',
         imageName: SparkleIcon,
         imageHD: SparkleIconHD,
     },
     {
-        key: 2,
-        photoID: '14_I2bj_uCGpDsnwqsUM8RJuNXQY4DYYl',
         artist: 'X | Twitter @Pentaagonkaa',
         link: 'https://x.com/Pentaagonkaa',
         imageName: Maid,
         imageHD: MaidHD,
     },
     {
-        key: 3,
-        photoID: '1B7vJ2o_GpIT0kcqYEyPgbLi728-3pA_2',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
         imageName: HoodIcon,
         imageHD: HoodIconHD,
     },
     {
-        key: 4,
-        photoID: '1QeQWTTj2GM6G7bsCG-H-KpKnU-RFXNhI',
         artist: 'X | Twitter @Beelzeryon',
         link: 'https://x.com/Beelzeryon',
         imageName: DemonSlayerCosplay,
         imageHD: DemonSlayerCosplayHD,
     },
     {
-        key: 5,
-        photoID: '1UrAufq6bNPLSEQ6GTo1bLXKA3KOC00Qr',
         artist: 'X | Twitter @NiixARTs',
         link: 'https://x.com/NiixARTs',
         imageName: TooCoolForSchool,
         imageHD: TooCoolForSchoolHD,
     },
     {
-        key: 6,
-        photoID: '1EvL7mUMtT07RCe9hv9_MaHseNW9Hcuat',
         artist: 'X | Twitter @foxynalie',
         link: 'https://x.com/foxynalie',
         imageName: CultOfTheGoat,
         imageHD: CultOfTheGoatHD,
     },
     {
-        key: 7,
-        photoID: '1GqBbR0lr79sT0eJICiZCmjKAHwkfqw_g',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
         imageName: GoatSticker,
         imageHD: GoatStickerHD,
     },
     {
-        key: 8,
-        photoID: '1Ipi_fjyaX2ma827XKpwCGWUga3_jz2l9',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
         imageName: AzaelReferenceSheets,
         imageHD: AzaelReferenceSheetsHD,
     },
     {
-        key: 9,
-        photoID: '1JIGxYjARSef5Mor86WCjuM3XGthI2ngi',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
         imageName: GoofyIcon,
         imageHD: GoofyIconHD,
     },
     {
-        key: 10,
-        photoID: '1Pk8jWzMrqCGNdUA3Bo7onXUE_lIHXNCX',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
         imageName: BiPrideIcon,
         imageHD: BiPrideIconHD,
     },
     {
-        key: 11,
-        photoID: '1M42J2sPGUMLbX1OST5Q62VmMd3z5pKbN',
         artist: 'X | Twitter @CrowParadeArt',
         link: 'https://x.com/CrowParadeArt',
         imageName: SillyGoat,
         imageHD: SillyGoatHD,
     },
     {
-        key: 12,
-        photoID: '1_yHyU7WNKH2KD36yOi0JbLVRkrv9mv4t',
         artist: 'X | Twitter @LoveIock_',
         link: 'https://x.com/LoveIock_',
         imageName: AzaelHeadshot,
