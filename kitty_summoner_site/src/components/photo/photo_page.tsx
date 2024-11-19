262e5ed7d11ed611e692058ccc8c1b6406494e01
Your GitHub SatapasT <satapastanachotpaisit@gmail.com>
2024-11-19 02:06:36 +0000

    made it nicer to view

--- kitty_summoner_site/src/components/photo/photo_page.tsx
+++ kitty_summoner_site/src/components/photo/photo_page.tsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { photoPageImage } from "../../constants/photo_page_item";
 import EnlargeImageModal from "../modal/enlarge_image_modal";
-import LoadingSpinner from "../../assets/component/loading_spinner"
 import PageNavigator from "../image_navigation/page_navigator";
 import ImageAmountSelector from "../image_navigation/image_per_page";
-
-
+import GalleryLoader from "../image_navigation/gallary_loader";
 
 const PhotoPage: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -55,44 +53,22 @@ const PhotoPage: React.FC = () => {
 
   return (
     <div className="container mx-auto px-5 py-1 lg:px-32 lg:pt-2 min-h-screen flex flex-col">
-      <div className="flex flex-col items-center justify-start mb-4 flex-grow">
+      <div className="flex flex-col items-center justify-start mb-4 flex-grow pb-8">
         <div className="flex">
           <ImageAmountSelector setItemsPerPage={setItemsPerPage}/>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-4">
-          {currentItems.map((item) => (
-            <div key={item.key} className="relative overflow-hidden p-1 bg-gradient-to-br from-red-400 via-orange-400 to-yellow-400 flex items-center justify-center">
-              {pageLoading && !loadedItems.has(item.key) ? (
-                <div className="w-full h-full flex items-center justify-center bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded shadow mt-2">
-                  <LoadingSpinner />
-                </div>
-              ) : (
-                <div className="flex flex-col items-center justify-center">
-                  <div className="flex items-center justify-center h-full">
-                    <a href='javascript:void(0)'>
-                      <img
-                        alt="gallery"
-                        className="w-full h-full object-contain rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-105"
-                        src={item.imageName}
-                        onClick={() => selectEnlargeImage(item.imageHD)}
-                      />
-                    </a>
-                  </div>
-                  <div className="text-center font-bold mt-2 transition-transform duration-300 ease-in-out transform hover:scale-105">
-                      {item.tag}
-                  </div>
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
+        <GalleryLoader
+        currentItems={currentItems}
+        pageLoading={pageLoading}
+        loadedItems={loadedItems}
+        selectEnlargeImage={selectEnlargeImage}
+      />
       </div>
         <PageNavigator
           totalPage={totalPage}
           currentPage={currentPage}
           setPageChange={setCurrentPage}
         />
-      <div className='py-'></div>
       <EnlargeImageModal 
       image={selectedEnlargedImage} 
       isVisible={enlargeImageVisible} 
--- kitty_summoner_site/src/constants/photo_page_item.tsx
+++ kitty_summoner_site/src/constants/photo_page_item.tsx
@@ -41,128 +41,145 @@ import November2024_19HD from "../assets/photos_images/november_2024_19.jpg";
 import November2024_20HD from "../assets/photos_images/november_2024_20.jpg";
 
 
-
-
 export const photoPageImage = [
     {
         key: 14,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_15,
         imageHD: November2024_15HD,
     },
     {
         key: 15,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_16,
         imageHD: November2024_16HD,
     },
     {
         key: 16,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_17,
         imageHD: November2024_17HD,
     },
     {
         key: 17,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_18,
         imageHD: November2024_18HD,
     },
     {
         key: 18,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_19,
         imageHD: November2024_19HD,
     },
     {
         key: 13,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_14,
         imageHD: November2024_14HD,
     },
     {
         key: 19,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_20,
         imageHD: November2024_20HD,
     },
     {
         key: 0,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_1,
         imageHD: November2024_1HD,
     },
     {
         key: 1,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_2,
         imageHD: November2024_2HD,
     },
     {
         key: 2,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_3,
         imageHD: November2024_3HD,
     },
     {
         key: 3,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_4,
         imageHD: November2024_4HD,
     },
     {
         key: 4,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_5,
         imageHD: November2024_5HD,
     },
     {
         key: 5,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_6,
         imageHD: November2024_6HD,
     },
     {
         key: 6,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_7,
         imageHD: November2024_7HD,
     },
     {
         key: 7,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_8,
         imageHD: November2024_8HD,
     },
     {
         key: 8,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_9,
         imageHD: November2024_9HD,
     },
     {
         key: 9,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_10,
         imageHD: November2024_10HD,
     },
     {
         key: 10,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_11,
         imageHD: November2024_11HD,
     },
     {
         key: 11,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_12,
         imageHD: November2024_12HD,
     },
     {
         key: 12,
         tag: 'November 2024',
+        link: 'NONE',
         imageName: November2024_13,
         imageHD: November2024_13HD,
     },
-    
 ];
