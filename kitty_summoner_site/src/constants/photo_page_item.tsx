a47b09cc2daa170b1e8e5e6f1616d207df4d6aca
Your GitHub SatapasT <satapastanachotpaisit@gmail.com>
2024-12-15 02:29:30 +0000

    added november newcastle meet images:

--- kitty_summoner_site/src/constants/photo_page_item.tsx
+++ kitty_summoner_site/src/constants/photo_page_item.tsx
@@ -1,182 +1,251 @@
-import November2024_1 from "../assets/photos_images/november_2024_1.webp";
-import November2024_2 from "../assets/photos_images/november_2024_2.webp";
-import November2024_3 from "../assets/photos_images/november_2024_3.webp";
-import November2024_4 from "../assets/photos_images/november_2024_4.webp";
-import November2024_5 from "../assets/photos_images/november_2024_5.webp";
-import November2024_6 from "../assets/photos_images/november_2024_6.webp";
-import November2024_7 from "../assets/photos_images/november_2024_7.webp";
-import November2024_8 from "../assets/photos_images/november_2024_8.webp";
-import November2024_9 from "../assets/photos_images/november_2024_9.webp";
-import November2024_10 from "../assets/photos_images/november_2024_10.webp";
-import November2024_11 from "../assets/photos_images/november_2024_11.webp";
-import November2024_12 from "../assets/photos_images/november_2024_12.webp";
-import November2024_13 from "../assets/photos_images/november_2024_13.webp";
-import November2024_14 from "../assets/photos_images/november_2024_14.webp";
-import November2024_15 from "../assets/photos_images/november_2024_15.webp";
-import November2024_16 from "../assets/photos_images/november_2024_16.webp";
-import November2024_17 from "../assets/photos_images/november_2024_17.webp";
-import November2024_18 from "../assets/photos_images/november_2024_18.webp";
-import November2024_19 from "../assets/photos_images/november_2024_19.webp";
-import November2024_20 from "../assets/photos_images/november_2024_20.webp";
+import November2024_1 from "../assets/photos_images/november_2024/november_2024_1.webp";
+import November2024_2 from "../assets/photos_images/november_2024/november_2024_2.webp";
+import November2024_3 from "../assets/photos_images/november_2024/november_2024_3.webp";
+import November2024_4 from "../assets/photos_images/november_2024/november_2024_4.webp";
+import November2024_5 from "../assets/photos_images/november_2024/november_2024_5.webp";
+import November2024_6 from "../assets/photos_images/november_2024/november_2024_6.webp";
+import November2024_7 from "../assets/photos_images/november_2024/november_2024_7.webp";
+import November2024_8 from "../assets/photos_images/november_2024/november_2024_8.webp";
+import November2024_9 from "../assets/photos_images/november_2024/november_2024_9.webp";
+import November2024_10 from "../assets/photos_images/november_2024/november_2024_10.webp";
+import November2024_11 from "../assets/photos_images/november_2024/november_2024_11.webp";
+import November2024_12 from "../assets/photos_images/november_2024/november_2024_12.webp";
+import November2024_13 from "../assets/photos_images/november_2024/november_2024_13.webp";
+import November2024_14 from "../assets/photos_images/november_2024/november_2024_14.webp";
+import November2024_15 from "../assets/photos_images/november_2024/november_2024_15.webp";
+import November2024_16 from "../assets/photos_images/november_2024/november_2024_16.webp";
+import November2024_17 from "../assets/photos_images/november_2024/november_2024_17.webp";
+import November2024_18 from "../assets/photos_images/november_2024/november_2024_18.webp";
+import November2024_19 from "../assets/photos_images/november_2024/november_2024_19.webp";
+import November2024_20 from "../assets/photos_images/november_2024/november_2024_20.webp";
+import December2024Newcastle_1 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_1.webp";
+import December2024Newcastle_2 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_2.webp";
+import December2024Newcastle_3 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_3.webp";
+import December2024Newcastle_4 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_4.webp";
+import December2024Newcastle_5 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_5.webp";
+import December2024Newcastle_6 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_6.webp";
+import December2024Newcastle_7 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_7.webp";
+import December2024Newcastle_8 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_8.webp";
+import December2024Newcastle_9 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_9.webp";
+import December2024Newcastle_10 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_10.webp";
+import December2024Newcastle_11 from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_11.webp";
 
-import November2024_1HD from "../assets/photos_images/november_2024_1.jpg";
-import November2024_2HD from "../assets/photos_images/november_2024_2.jpg";
-import November2024_3HD from "../assets/photos_images/november_2024_3.jpg";
-import November2024_4HD from "../assets/photos_images/november_2024_4.jpg";
-import November2024_5HD from "../assets/photos_images/november_2024_5.jpg";
-import November2024_6HD from "../assets/photos_images/november_2024_6.jpg";
-import November2024_7HD from "../assets/photos_images/november_2024_7.jpg";
-import November2024_8HD from "../assets/photos_images/november_2024_8.jpg";
-import November2024_9HD from "../assets/photos_images/november_2024_9.jpg";
-import November2024_10HD from "../assets/photos_images/november_2024_10.jpg";
-import November2024_11HD from "../assets/photos_images/november_2024_11.jpg";
-import November2024_12HD from "../assets/photos_images/november_2024_12.jpg";
-import November2024_13HD from "../assets/photos_images/november_2024_13.jpg";
-import November2024_14HD from "../assets/photos_images/november_2024_14.jpg";
-import November2024_15HD from "../assets/photos_images/november_2024_15.jpg";
-import November2024_16HD from "../assets/photos_images/november_2024_16.jpg";
-import November2024_17HD from "../assets/photos_images/november_2024_17.jpg";
-import November2024_18HD from "../assets/photos_images/november_2024_18.jpg";
-import November2024_19HD from "../assets/photos_images/november_2024_19.jpg";
-import November2024_20HD from "../assets/photos_images/november_2024_20.jpg";
 
+
+import November2024_1HD from "../assets/photos_images/november_2024/november_2024_1.jpg";
+import November2024_2HD from "../assets/photos_images/november_2024/november_2024_2.jpg";
+import November2024_3HD from "../assets/photos_images/november_2024/november_2024_3.jpg";
+import November2024_4HD from "../assets/photos_images/november_2024/november_2024_4.jpg";
+import November2024_5HD from "../assets/photos_images/november_2024/november_2024_5.jpg";
+import November2024_6HD from "../assets/photos_images/november_2024/november_2024_6.jpg";
+import November2024_7HD from "../assets/photos_images/november_2024/november_2024_7.jpg";
+import November2024_8HD from "../assets/photos_images/november_2024/november_2024_8.jpg";
+import November2024_9HD from "../assets/photos_images/november_2024/november_2024_9.jpg";
+import November2024_10HD from "../assets/photos_images/november_2024/november_2024_10.jpg";
+import November2024_11HD from "../assets/photos_images/november_2024/november_2024_11.jpg";
+import November2024_12HD from "../assets/photos_images/november_2024/november_2024_12.jpg";
+import November2024_13HD from "../assets/photos_images/november_2024/november_2024_13.jpg";
+import November2024_14HD from "../assets/photos_images/november_2024/november_2024_14.jpg";
+import November2024_15HD from "../assets/photos_images/november_2024/november_2024_15.jpg";
+import November2024_16HD from "../assets/photos_images/november_2024/november_2024_16.jpg";
+import November2024_17HD from "../assets/photos_images/november_2024/november_2024_17.jpg";
+import November2024_18HD from "../assets/photos_images/november_2024/november_2024_18.jpg";
+import November2024_19HD from "../assets/photos_images/november_2024/november_2024_19.jpg";
+import November2024_20HD from "../assets/photos_images/november_2024/november_2024_20.jpg";
+import December2024Newcastle_1HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_1.jpg";
+import December2024Newcastle_2HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_2.jpg";
+import December2024Newcastle_3HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_3.jpg";
+import December2024Newcastle_4HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_4.jpg";
+import December2024Newcastle_5HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_5.jpg";
+import December2024Newcastle_6HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_6.jpg";
+import December2024Newcastle_7HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_7.jpg";
+import December2024Newcastle_8HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_8.jpg";
+import December2024Newcastle_9HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_9.jpg";
+import December2024Newcastle_10HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_10.jpg";
+import December2024Newcastle_11HD from "../assets/photos_images/december_2024_newcastle/december_2024_newcastle_11.jpg";
 
 export const photoPageImage = [
     {
-        key: 14,
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_1,
+        imageHD: December2024Newcastle_1HD,
+    },
+    {
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_2,
+        imageHD: December2024Newcastle_2HD,
+    },
+    {
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_3,
+        imageHD: December2024Newcastle_3HD,
+    },
+    {
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_4,
+        imageHD: December2024Newcastle_4HD,
+    },
+    {
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_5,
+        imageHD: December2024Newcastle_5HD,
+    },
+    {
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_6,
+        imageHD: December2024Newcastle_6HD,
+    },
+    {
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_7,
+        imageHD: December2024Newcastle_7HD,
+    },
+    {
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_8,
+        imageHD: December2024Newcastle_8HD,
+    },
+    {
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_9,
+        imageHD: December2024Newcastle_9HD,
+    },
+    {
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_10,
+        imageHD: December2024Newcastle_10HD,
+    },
+    {
+        tag: 'December 2024 Newcastle',
+        link: 'NONE',
+        imageName: December2024Newcastle_11,
+        imageHD: December2024Newcastle_11HD,
+    },
+    {
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_15,
         imageHD: November2024_15HD,
     },
     {
-        key: 15,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_16,
         imageHD: November2024_16HD,
     },
     {
-        key: 16,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_17,
         imageHD: November2024_17HD,
     },
     {
-        key: 17,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_18,
         imageHD: November2024_18HD,
     },
     {
-        key: 18,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_19,
         imageHD: November2024_19HD,
     },
     {
-        key: 13,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_14,
         imageHD: November2024_14HD,
     },
     {
-        key: 19,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_20,
         imageHD: November2024_20HD,
     },
     {
-        key: 0,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_1,
         imageHD: November2024_1HD,
     },
     {
-        key: 1,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_2,
         imageHD: November2024_2HD,
     },
     {
-        key: 2,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_3,
         imageHD: November2024_3HD,
     },
     {
-        key: 3,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_4,
         imageHD: November2024_4HD,
     },
     {
-        key: 4,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_5,
         imageHD: November2024_5HD,
     },
     {
-        key: 5,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_6,
         imageHD: November2024_6HD,
     },
     {
-        key: 6,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_7,
         imageHD: November2024_7HD,
     },
     {
-        key: 7,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_8,
         imageHD: November2024_8HD,
     },
     {
-        key: 8,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_9,
         imageHD: November2024_9HD,
     },
     {
-        key: 9,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_10,
         imageHD: November2024_10HD,
     },
     {
-        key: 10,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_11,
         imageHD: November2024_11HD,
     },
     {
-        key: 11,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_12,
         imageHD: November2024_12HD,
     },
     {
-        key: 12,
         tag: 'November 2024',
         link: 'NONE',
         imageName: November2024_13,
